317ef09cfa30102040a4747b5ec982fc6270a1ae
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-03-06 02:03:14 +0530

    added prettier

--- kmaptest.c
+++ kmaptest.c
@@ -2,143 +2,165 @@
 int arr[8];
 int input[8];
 int size = 0;
-int quad,octet=0;
-int quad5val[3],quad6val[3];
-int sizearr(int size) {
+int quad, octet = 0;
+int quad5val[3], quad6val[3];
+int sizearr(int size)
+{
     return size;
 }
-// storing input in array fo map
-void storing(int input[], int size) {
+// storing input in array foR map
+void storing(int input[], int size)
+{
     int i, j = 0;
-    for (i = 0; i < 8; i++) {
-        if (j < size) {
-            if (input[j] == i) {
+    for (i = 0; i < 8; i++)
+    {
+        if (j < size)
+        {
+            if (input[j] == i)
+            {
                 arr[i] = 1;
-                j++; 
-            } else {
+                j++;
+            }
+            else
+            {
                 arr[i] = 0;
             }
-        } else {
+        }
+        else
+        {
             arr[i] = 0;
         }
     }
 }
 
-void display() {
+void display()
+{
     int i = 0;
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         printf("%d \t", arr[i]);
     }
 }
-int quadandoctet(int arr[]){
-int i,j,k=0;
-int mid =4;
-int start =0;
-int end=8;
-int quad1=0;
-int quad2=0;
-int quad3=0;
-int quad4 =0;
-int quad5=0;
-int quad6=0;
-printf("\n%d\n",quad1);
-
-for ( j = 0; j < mid; j++)
+int quadandoctet(int arr[])
 {
-    // center and roll map
-    if (arr[2*j] == 1){
-        quad1++;
-        if(quad1==4){
-            quad++;
+    int i, j, k = 0;
+    int mid = 4;
+    int start = 0;
+    int end = 8;
+    int quad1 = 0;
+    int quad2 = 0;
+    int quad3 = 0;
+    int quad4 = 0;
+    int quad5 = 0;
+    int quad6 = 0;
+    printf("\n%d\n", quad1);
+
+    for (j = 0; j < mid; j++)
+    {
+        // center and roll map
+        if (arr[2 * j] == 1)
+        {
+            quad1++;
+            if (quad1 == 4)
+            {
+                quad++;
+            }
+        }
+        if (arr[2 * j + 1] == 1)
+        {
+            quad2++;
+            if (quad2 == 4)
+            {
+                quad++;
+            }
+        }
+        //  checking rows
+        if (arr[j] == 1)
+        {
+            quad3++;
+            if (quad3 == 4)
+            {
+                quad++;
+            }
+        }
+        if (arr[j + 4] == 1)
+        {
+            quad4++;
+            if (quad4 == 4)
+            {
+                quad++;
+            }
         }
     }
-    if (arr[2*j+1] == 1){
-        quad2++;
-        if(quad2==4){
-            quad++;
+    for (k = 1; k <= 2; k++)
+    {
+        //   checking 2 vetical halves of map
+        if (arr[end - k] == 1 && arr[mid - k] == 1)
+        {
+            quad5++;
+            if (quad5 == 2)
+            {
+                quad++;
+            }
+        }
+        if (arr[start + (k - 1)] == 1 && arr[mid + (k - 1)] == 1)
+        {
+            quad6++;
+            if (quad6 == 2)
+            {
+                quad++;
+            }
         }
     }
-//  checking rows 
-    if(arr[j]==1){
-        quad3++;
-           if(quad3==4){
-            quad++;
-        }
-    }
-    if(arr[j+4]==1){
-        quad4++;
-           if(quad4==4){
-            quad++;
-        }
-    }
+    printf("quad1 :%d\t", quad1);
+    printf("quad2 :%d\t", quad2);
+    printf("quad3 :%d\t", quad3);
+    printf("quad4 :%d\t", quad4);
+    printf("quad5 :%d\t", quad5);
+    printf("quad6 :%d\t", quad6);
+    return quad;
 }
-for ( k = 1; k <= 2; k++)
+// Starting pairs
+int pairs(int arr[], int input[])
 {
-//   checking 2 vetical halves of map
-    if(arr[end-k]==1 && arr[mid-k]==1){
-        quad5++;
-          if(quad5==2){
-            quad++;
-        } 
-    }
-    if(arr[start+(k-1)]==1 && arr[mid+(k-1)]==1){
-        quad6++;
-          if(quad6==2){
-            quad++;
-        } 
-    }
-}
-printf("quad1 :%d",quad1);
-printf("quad2 :%d",quad2);
-printf("quad3 :%d",quad3);
-printf("quad4 :%d",quad4);
-printf("quad5 :%d",quad5);
-printf("quad6 :%d",quad6);
-    printf("quad 5 values : \n");
-for(int o =0 ; quad5val[o]!='\0';i++){
-    printf("%d\t",quad5val[o]);
-}
-
-
-
-
-return quad;
-
-
-}
-//Starting pairs
-int pairs(int arr[],int input[]){
-    if(quad==0){
- 
+    if (quad == 0)
+    {
     }
 }
 
-int main() {
+int main()
+{
     int i = 0;
     char l;
     printf("Enter the values of the input array , in the start press n :\n");
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         printf("To terminate input press y \n");
         scanf("%c", &l);
-        if (l == 'y') {
+        if (l == 'y')
+        {
             break;
-        } else {
+        }
+        else
+        {
             printf("Enter input %d\n", i);
             scanf("%d", &input[i]);
             size++;
         }
     }
-printf("%d\n",size);
+    printf("%d\n", size);
     storing(input, size);
     display();
-    int q=quadandoctet(arr);
-    if(q<6){
+    int q = quadandoctet(arr);
+    if (q < 6)
+    {
 
- printf("\nNumber of quads are : %d ",q);
+        printf("\nNumber of quads are : %d ", q);
     }
-   else{
-    printf("Number of octets are : 1\n");
-   }
-printf("quad : %d",quad);
+    else
+    {
+        printf("Number of octets are : 1\n");
+    }
+    printf("quad : %d", quad);
     return 0;
 }
